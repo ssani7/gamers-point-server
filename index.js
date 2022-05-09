c0e98887f74c72682990dd6d7c9c2d4e367abbb1
ssani7 <sanaullah.sani756@gmail.com>
2022-05-09 05:15:32 -0600

    added deliver and update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -22,6 +22,39 @@ const run = async () => {
             const cursor = gpuCollection.find({});
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/gpu/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await gpuCollection.findOne({ _id: ObjectId(id) });
+            res.send(result);
+        })
+
+        app.put('/restock/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedGpu = req.body;
+            const newGpu = {
+                $set: {
+                    quantity: updatedGpu.quantity
+                }
+            }
+            const result = await gpuCollection.updateOne({ _id: ObjectId(id) }, newGpu, { upsert: true });
+            res.send(result);
+
+        })
+
+        app.put('/deliver/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedGpu = req.body;
+            const newGpu = {
+                $set: {
+                    quantity: updatedGpu.quantity,
+                    sold: updatedGpu.sold
+                }
+            }
+            const result = await gpuCollection.updateOne({ _id: ObjectId(id) }, newGpu, { upsert: true });
+            res.send(result);
+
         })
     }
     finally {
